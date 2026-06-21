499a330a7a8e81533b55e64d4c16210e100acbd1
masmet99 <laodemasisi99@gmail.com>
2026-06-21 14:39:10 +0800

    Use headless OpenCV

--- backend/services/face_service.py
+++ backend/services/face_service.py
@@ -4,22 +4,45 @@ import numpy as np
 
 app = FaceAnalysis()
 
-try:
+app.prepare(
+    ctx_id=-1,
+    det_size=(640, 640)
+)
 
-    app.prepare(
-        ctx_id=0,
-        det_size=(640, 640)
+print("InsightFace CPU Loaded")
+
+
+def extract_embedding(image_path):
+
+    image = cv2.imread(image_path)
+
+    if image is None:
+        return None
+
+    faces = app.get(image)
+
+    if len(faces) == 0:
+        return None
+
+    embedding = faces[0].embedding
+
+    return embedding.tolist()
+
+
+def compare_embeddings(
+    embedding1,
+    embedding2
+):
+
+    embedding1 = np.array(embedding1)
+    embedding2 = np.array(embedding2)
+
+    similarity = np.dot(
+        embedding1,
+        embedding2
+    ) / (
+        np.linalg.norm(embedding1)
+        * np.linalg.norm(embedding2)
     )
 
-    print("InsightFace GPU Loaded")
-
-except Exception as e:
-
-    print("GPU NOT AVAILABLE:", e)
-
-    app.prepare(
-        ctx_id=-1,
-        det_size=(640, 640)
-    )
-
-    print("InsightFace CPU Loaded")
+    return float(similarity)
