252af8fe938bd6d658fe5bb408f814ddeffb7508
masmet99 <laodemasisi99@gmail.com>
2026-06-21 18:46:32 +0800

    create tables automatically

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,18 @@ from routes.auth import router as auth_router
 from routes.attendance import router as attendance_router
 from fastapi.middleware.cors import CORSMiddleware
 
+from database.base import Base
+from database.connection import engine
+
+from models.user import User
+from models.attendance import Attendance
+from models.face_embedding import FaceEmbedding
+from models.geofence import Geofence
+
+
+Base.metadata.create_all(bind=engine)
+print("ALL TABLES CREATED")
+
 app = FastAPI()
 
 app.add_middleware(
