7aba81b0131370129d48fba193fd7c8d908d684e
masmet99 <laodemasisi99@gmail.com>
2026-06-21 14:50:22 +0800

    test without insightface

--- backend/services/face_service.py
+++ backend/services/face_service.py
@@ -1,32 +1,13 @@
-from insightface.app import FaceAnalysis
-import cv2
 import numpy as np
 
-app = FaceAnalysis()
-
-app.prepare(
-    ctx_id=-1,
-    det_size=(640, 640)
-)
-
-print("InsightFace CPU Loaded")
+print("FACE SERVICE LOADED")
 
 
 def extract_embedding(image_path):
 
-    image = cv2.imread(image_path)
+    print("DUMMY EMBEDDING")
 
-    if image is None:
-        return None
-
-    faces = app.get(image)
-
-    if len(faces) == 0:
-        return None
-
-    embedding = faces[0].embedding
-
-    return embedding.tolist()
+    return [0.1] * 512
 
 
 def compare_embeddings(
@@ -34,15 +15,4 @@ def compare_embeddings(
     embedding2
 ):
 
-    embedding1 = np.array(embedding1)
-    embedding2 = np.array(embedding2)
-
-    similarity = np.dot(
-        embedding1,
-        embedding2
-    ) / (
-        np.linalg.norm(embedding1)
-        * np.linalg.norm(embedding2)
-    )
-
-    return float(similarity)
+    return 0.9
